d3bdf6a6cf7f1ebe24f285236b1966181f0447d9
unimatrix2 <dnny.nis@gmail.com>
2022-02-22 20:40:20 -0300

    added navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
@@ -5,50 +6,46 @@ import Typography from '@mui/material/Typography';
 import MainAppBar from '../components/AppBar';
 
 export default function Home() {
-	/* const useStyles = makeStyles((theme) => ({
-		
-	}));
-
-	const classes = useStyles(); */
-
-  const classes = {
-    card: {
-      width: 500,
-      height: 500,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
-    },
-    container: {
-      display: 'flex',
-      justifyContent: 'space-evenly',
-      alignItems: 'center',
-      height: 'calc(100vh - 64px)'
-    },
-  }
+	const classes = {
+		card: {
+			width: 500,
+			height: 500,
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'space-evenly',
+			cursor: 'pointer'
+		},
+		container: {
+			display: 'flex',
+			justifyContent: 'space-evenly',
+			alignItems: 'center',
+			height: 'calc(100vh - 64px)',
+		},
+	};
 
 	return (
 		<>
 			<MainAppBar />
-			<Container
-        maxWidth='false'
-        sx={classes.container}
-      >
+			<Container maxWidth="false" sx={classes.container}>
+				<Link href="/provider/register">
 					<Paper elevation={3} sx={classes.card}>
 						<Typography variant="h2" align="center">
 							Cadastrar um Fornecedor
 						</Typography>
 					</Paper>
-					<Paper elevation={3} sx={classes.card}>
-						<Typography variant="h2" align="center">
-							Listar Forncecedores Cadastrados
-						</Typography>
-					</Paper>
-					<Paper elevation={3} sx={classes.card}>
-						<Typography variant="h2" align="center">
-							Listar um Fornecedor Cadastrado
-						</Typography>
-					</Paper>
+				</Link>
+				<Link href="/provider/list">
+				<Paper elevation={3} sx={classes.card}>
+					<Typography variant="h2" align="center">
+						Listar Forncecedores Cadastrados
+					</Typography>
+				</Paper>
+				</Link>
+				<Paper elevation={3} sx={classes.card}>
+					<Typography variant="h2" align="center">
+						Listar um Fornecedor Cadastrado
+					</Typography>
+				</Paper>
 			</Container>
 		</>
 	);
