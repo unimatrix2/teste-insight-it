cf4ee9c27373d1e449b83fb85082b013f4958ba1
unimatrix2 <dnny.nis@gmail.com>
2022-02-22 11:14:37 -0300

    disabled emotion cache bc it was causing problems

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,21 @@
 import PropTypes from 'prop-types';
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+// import { CacheProvider } from '@emotion/react';
+import ThemeProvider from '@mui/styles/ThemeProvider';
 
-import createEmotionCache from '../util/emotionCache';
+// import createEmotionCache from '../util/emotionCache';
 import lightTheme from '../styles/theme/lightTheme';
 
-const clientSideCache = createEmotionCache();
+// const clientSideCache = createEmotionCache();
 
-function MyApp({ Component, emotionCache = clientSideCache, pageProps }) {
+function MyApp({ Component,/*  emotionCache = clientSideCache, */ pageProps }) {
 	return (
-		<CacheProvider value={emotionCache}>
+		// <CacheProvider value={emotionCache}>
 			<ThemeProvider theme={lightTheme}>
 				<CssBaseline />
 				<Component {...pageProps} />
 			</ThemeProvider>
-		</CacheProvider>
+		// </CacheProvider>
 	);
 }
 
@@ -22,6 +23,6 @@ export default MyApp;
 
 MyApp.propTypes = {
 	Component: PropTypes.elementType.isRequired,
-	emotionCache: PropTypes.object,
+	// emotionCache: PropTypes.object,
 	pageProps: PropTypes.object.isRequired,
 };
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from '../util/emotionCache';
+// import createEmotionServer from '@emotion/server/create-instance';
+// import createEmotionCache from '../util/emotionCache';
+import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
 
 export default class MyDocument extends Document {
 	render() {
@@ -46,43 +47,48 @@ MyDocument.getInitialProps = async (ctx) => {
 	// 2. page.getInitialProps
 	// 3. app.render
 	// 4. page.render
-
+	const sheets = new ServerStyleSheets();
 	const originalRenderPage = ctx.renderPage;
 
 	// You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
 	// However, be aware that it can have global side effects.
-	const cache = createEmotionCache();
-	const { extractCriticalToChunks } = createEmotionServer(cache);
+	// const cache = createEmotionCache();
+	// const { extractCriticalToChunks } = createEmotionServer(cache);
 
 	/* eslint-disable */
-	ctx.renderPage = () =>
+	/* ctx.renderPage = () =>
 		originalRenderPage({
 			enhanceApp: (App) =>
 				function EnhanceApp(props) {
 					return <App emotionCache={cache} {...props} />;
 				},
+		}); */
+		ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
 		});
 	/* eslint-enable */
 
 	const initialProps = await Document.getInitialProps(ctx);
 	// This is important. It prevents emotion to render invalid HTML.
 	// See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-	const emotionStyles = extractCriticalToChunks(initialProps.html);
-	const emotionStyleTags = emotionStyles.styles.map((style) => (
+	// const emotionStyles = extractCriticalToChunks(initialProps.html);
+	/* const emotionStyleTags = emotionStyles.styles.map((style) => (
 		<style
 			data-emotion={`${style.key} ${style.ids.join(' ')}`}
 			key={style.key}
 			// eslint-disable-next-line react/no-danger
 			dangerouslySetInnerHTML={{ __html: style.css }}
 		/>
-	));
+	)); */
 
 	return {
 		...initialProps,
 		// Styles fragment is rendered after the app and page rendering finish.
 		styles: [
 			...React.Children.toArray(initialProps.styles),
-			...emotionStyleTags,
+			sheets.getStyleElement()
+			// ...emotionStyleTags,
 		],
 	};
 };
