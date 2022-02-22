110b18b20350d8616959f4807d3398a90b34810b
unimatrix2 <dnny.nis@gmail.com>
2022-02-22 20:36:56 -0300

    fixed connection config on db

--- middleware/db.middleware.js
+++ middleware/db.middleware.js
@@ -4,7 +4,6 @@ const dbConnect = handler => async (req, res) => {
 	if (connections[0].readyState === 1) return handler(req, res);
 
 	await connect(process.env.mongodburl, {
-		useCreateIndex: true,
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
