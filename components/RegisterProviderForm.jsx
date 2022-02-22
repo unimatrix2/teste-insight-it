5b78942fd29161419f351ac7d5ae9c1c1c794a97
unimatrix2 <dnny.nis@gmail.com>
2022-02-22 20:35:44 -0300

    added snackbar hoc & proper service call

--- components/RegisterProviderForm.jsx
+++ components/RegisterProviderForm.jsx
@@ -3,10 +3,11 @@ import Button from '@mui/material/Button';
 import { Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 
+import { withSnackBar } from './SnackBar';
+import { registerProvider } from '../services/Provider.service';
 import registerProviderSchema from '../validations/registerProviderSchema.validation';
-import registerProviderMapper from '../mappers/registerProviderMapper';
 
-export default function RegisterProviderForm() {
+function RegisterProviderForm({ snack }) {
 
 	const formik = useFormik({
 		initialValues: {
@@ -23,13 +24,11 @@ export default function RegisterProviderForm() {
 		},
 		validationSchema: registerProviderSchema,
 		onSubmit: async (values, helpers) => {
-			await fetch('/api/provider/register', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(registerProviderMapper(values))
-			});
+			await registerProvider(
+				values,
+				helpers,
+				snack
+			);
 		}
 	});
 
@@ -39,14 +38,14 @@ export default function RegisterProviderForm() {
 		<form onSubmit={formik.handleSubmit} noValidate>
 			<Typography variant='h4'>Dados Básicos</Typography>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="name"
-				label="Nome Fantasia"
-				name="name"
-				autoComplete="name"
+				id='name'
+				label='Nome Fantasia'
+				name='name'
+				autoComplete='name'
 				value={formik.values.name}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -54,14 +53,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.name && Boolean(formik.errors.name)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="officialRegistry"
-				label="Razão Social"
-				name="officialRegistry"
-				autoComplete="officialRegistry"
+				id='officialRegistry'
+				label='Razão Social'
+				name='officialRegistry'
+				autoComplete='officialRegistry'
 				value={formik.values.officialRegistry}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -69,14 +68,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.officialRegistry && Boolean(formik.errors.officialRegistry)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="cnpj"
-				label="CNPJ"
-				name="cnpj"
-				autoComplete="cnpj"
+				id='cnpj'
+				label='CNPJ'
+				name='cnpj'
+				autoComplete='cnpj'
 				value={formik.values.cnpj}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -84,14 +83,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.cnpj && Boolean(formik.errors.cnpj)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="segment"
-				label="Segmento"
-				name="segment"
-				autoComplete="segment"
+				id='segment'
+				label='Segmento'
+				name='segment'
+				autoComplete='segment'
 				value={formik.values.segment}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -100,14 +99,14 @@ export default function RegisterProviderForm() {
 			/>
 			<Typography variant='h4'>Endereço</Typography>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="postalCode"
-				label="CEP"
-				name="postalCode"
-				autoComplete="postalCode"
+				id='postalCode'
+				label='CEP'
+				name='postalCode'
+				autoComplete='postalCode'
 				value={formik.values.postalCode}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -115,14 +114,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.postalCode && Boolean(formik.errors.postalCode)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="street"
-				label="Rua"
-				name="street"
-				autoComplete="street"
+				id='street'
+				label='Rua'
+				name='street'
+				autoComplete='street'
 				value={formik.values.street}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -130,14 +129,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.street && Boolean(formik.errors.street)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="number"
-				label="Número"
-				name="number"
-				autoComplete="number"
+				id='number'
+				label='Número'
+				name='number'
+				autoComplete='number'
 				value={formik.values.number}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -145,13 +144,13 @@ export default function RegisterProviderForm() {
 				error={formik.touched.number && Boolean(formik.errors.number)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				fullWidth
-				id="complement"
-				label="Complemento"
-				name="complement"
-				autoComplete="complement"
+				id='complement'
+				label='Complemento'
+				name='complement'
+				autoComplete='complement'
 				value={formik.values.complement}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -160,14 +159,14 @@ export default function RegisterProviderForm() {
 			/>
 			<Typography variant='h4'>Contato</Typography>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="telephone"
-				label="Telefone"
-				name="telephone"
-				autoComplete="telephone"
+				id='telephone'
+				label='Telefone'
+				name='telephone'
+				autoComplete='telephone'
 				value={formik.values.telephone}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -175,14 +174,14 @@ export default function RegisterProviderForm() {
 				error={formik.touched.telephone && Boolean(formik.errors.telephone)}
 			/>
 			<TextField
-				variant="outlined"
-				margin="normal"
+				variant='outlined'
+				margin='normal'
 				required
 				fullWidth
-				id="email"
-				label="E-Mail"
-				name="email"
-				autoComplete="email"
+				id='email'
+				label='E-Mail'
+				name='email'
+				autoComplete='email'
 				value={formik.values.email}
 				onChange={formik.handleChange}
 				onBlur={formik.handleBlur}
@@ -190,10 +189,10 @@ export default function RegisterProviderForm() {
 				error={formik.touched.email && Boolean(formik.errors.email)}
 			/>
 			<Button
-				type="submit"
+				type='submit'
 				fullWidth
-				variant="contained"
-				color="primary"
+				variant='contained'
+				color='primary'
 			>
 				Cadastrar Fornecedor
 			</Button>
@@ -201,3 +200,5 @@ export default function RegisterProviderForm() {
 		</>
 	)
 }
+
+export default withSnackBar(RegisterProviderForm);
