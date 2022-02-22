4b1e45d209d921d7abdea7c0923026176a5300a4
unimatrix2 <dnny.nis@gmail.com>
2022-02-22 20:38:45 -0300

    added proper error handling

--- repositories/Provider.repository.js
+++ repositories/Provider.repository.js
@@ -1,4 +1,5 @@
 import AppError from '../error/AppError';
+import providerCreateErrorMapper from '../mappers/registerProviderErrorMapper';
 import Provider from '../models/Provider.model';
 
 export const insertProvider = async (provider) => {
@@ -6,9 +7,22 @@ export const insertProvider = async (provider) => {
 		const newProvider = new Provider(provider);
 		await newProvider.save();
 	} catch (error) {
+		// O upperCase não está no mapper pois o mapper é genérico, apenas
+		// o campo de CNPJ precisa ser uppercase. Como somente esse campo é
+		// indexado na base de dados, apenas nesse pode ocorrer um erro de
+		// criação, uma vez que quaisquer inconsistências serão tratadas na
+		// validação de dados no formulário
+		if (error.keyValue) {
+			const errorKey = providerCreateErrorMapper(error);
+			throw new AppError({
+				message: `${errorKey.toUpperCase()} já existe na base de dados`,
+				type: `Provider-Create-${errorKey.toUpperCase()}`,
+				status: 400
+			});
+		}
 		throw new AppError({
 			message: 'Ocorreu um erro ao criar o fornecedor, tente novamente mais tarde',
-			type: 'insertProvider-Repo',
+			type: 'Provider-Create-Repository',
 			status: 500
 		});
 	}
