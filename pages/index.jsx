d0f0c4844754858133e9a72f957d4a23bcb32ef6
unimatrix2 <dnny.nis@gmail.com>
2022-02-23 01:16:49 -0300

    implemented search dialog

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
 import MainAppBar from '../components/AppBar';
+import ProviderSearchDialogForm from '../components/forms/ProviderSearchDialogFrom';
 
 export default function Home() {
+	const [showDialog, toggleShowDialog] = useState(false);
+
 	const classes = {
 		card: {
 			width: 500,
@@ -27,26 +31,29 @@ export default function Home() {
 		<>
 			<MainAppBar />
 			<Container maxWidth="false" sx={classes.container}>
-				<Link href="/provider/register">
+				<Link href="/provider/register" passHref>
 					<Paper elevation={3} sx={classes.card}>
 						<Typography variant="h2" align="center">
 							Cadastrar um Fornecedor
 						</Typography>
 					</Paper>
 				</Link>
-				<Link href="/provider/list">
+				<Link href="/provider/list" passHref>
 				<Paper elevation={3} sx={classes.card}>
 					<Typography variant="h2" align="center">
 						Listar Forncecedores Cadastrados
 					</Typography>
 				</Paper>
 				</Link>
-				<Paper elevation={3} sx={classes.card}>
-					<Typography variant="h2" align="center">
-						Listar um Fornecedor Cadastrado
-					</Typography>
-				</Paper>
+				<div onClick={() => toggleShowDialog(true)}>
+					<Paper elevation={3} sx={classes.card}>
+						<Typography variant="h2" align="center">
+							Listar um Fornecedor Cadastrado
+						</Typography>
+					</Paper>
+				</div>
 			</Container>
+			{showDialog ? <ProviderSearchDialogForm open={showDialog} toggle={toggleShowDialog} /> : null}
 		</>
 	);
 }
