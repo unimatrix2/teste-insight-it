f894071698224fda5aac4c8b71705c2a77ff6efa
unimatrix2 <dnny.nis@gmail.com>
2022-02-23 01:47:51 -0300

    better validation

--- validations/registerProviderSchema.validation.js
+++ validations/registerProviderSchema.validation.js
@@ -16,6 +16,7 @@ const registerProviderSchema = yup.object({
         .trim()
         .min(14, 'Ao menos 14 caracteres')
         .max(14, 'No máximo 14 caracteres')
+        .matches(/[0-9]/, 'Apenas números')
         .required('Campo obrigatório'),
     segment: yup
         .string()
@@ -27,6 +28,7 @@ const registerProviderSchema = yup.object({
         .trim()
         .min(8, 'Ao menos 8 caracteres')
         .max(8, 'No máximo 8 caracteres')
+        .matches(/[0-9]/, 'Apenas números')
         .required('Campo obrigatório'),
     street: yup
         .string()
@@ -46,6 +48,7 @@ const registerProviderSchema = yup.object({
         .trim()
         .min(10, 'Ao menos 10 caracteres')
         .max(11, 'No máximo 11 caracteres')
+        .matches(/[0-9]/, 'Apenas números')
         .required('Campo obrigatório'),
     email: yup
         .string()
